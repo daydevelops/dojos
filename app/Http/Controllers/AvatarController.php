8a378717b558e659a141be3caa29965f63ed2b2e
daydevelops <adamday@daydevelops.com>
2020-11-25 10:49:56 -0330

    old dojo image deleted upon uploading new

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Dojo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Validation\Rule;
 
 class AvatarController extends Controller
@@ -20,9 +21,11 @@ class AvatarController extends Controller
             ]
         ]);
         $dojo = Dojo::find($data['dojo']);
+        $old_image = $dojo->image;
         $dojo->update([
             'image' => 'storage/' . request()->file('image')->store('images','public')
         ]);
+        File::delete(public_path($old_image));
         return $dojo->fresh()->image;
     }
 }
