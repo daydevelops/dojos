c8c121ef43af021ba6f132434cc9ce29ba41891a
daydevelops <adamday@daydevelops.com>
2020-12-01 10:02:02 -0330

    installed dusk with initial tests

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /** @test */
+    public function a_user_can_login() {
+        
+    }
+
+    /** @test */
+    public function a_user_can_logout() {
+        
+    }
+
+    /** @test */
+    public function a_guest_can_register() {
+        
+    }
+}
