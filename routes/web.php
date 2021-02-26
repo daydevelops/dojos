8f0effba5fc135f86ea403a5cb77ab38bb18cc04
adam day <adamday@daydevelops.com>
2021-02-26 17:08:01 -0330

    add route for billing portal

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Symfony\Component\HttpFoundation\Request;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/billing',function (Request $request) {
+    return $request->user()->redirectToBillingPortal();
+});
