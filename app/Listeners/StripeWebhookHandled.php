72293a00653c24bb6f6836959b25ec101b693c12
adam day <adamday@daydevelops.com>
2021-08-01 13:59:39 -0230

    refactored stripe webhook listener

--- app/Listeners/StripeWebhookHandled.php
+++ app/Listeners/StripeWebhookHandled.php
@@ -31,27 +31,26 @@ class StripeWebhookHandled
 
         // after stripe handles the webhook, we need to update the dojo information and notify the user 
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-        // Retrieve the request's body and parse it as JSON
-        $input = @file_get_contents("php://input");
-        $event_json = json_decode($input);
-        $event = \Stripe\Event::retrieve($event_json->id);
+        $event = $event->payload;
 
         // only listening for subscription events
-        if (substr($event->type,0,21) != 'customer.subscription') {
+        if (substr($event['type'],0,21) != 'customer.subscription') {
             return;
         }
 
-        $dojo_id = $event->data->object->metadata['dojo_id'];
-        $plan_id = $event->data->object->plan['id'];
+        $data = $event['data'];
+
+        $dojo_id = $data['object']['metadata']['dojo_id'];
+        $plan_id = $data['object']['plan']['id'];
         $plan = StripeProduct::where(['product_id' => $plan_id])->first();
         $dojo = Dojo::find($dojo_id);
         $user = $dojo->user;
-        if ($event->type == 'customer.subscription.deleted') {
+        if ($event['type'] == 'customer.subscription.deleted') {
             $subscription_id = null;
             $cost = null;
             $cycle = null;
         } else {
-            $subscription_id = DB::table('subscriptions')->where(['stripe_id'=>$event->data->object->id])->get()[0]->id;
+            $subscription_id = DB::table('subscriptions')->where(['stripe_id'=>$data['object']['id']])->get()[0]->id;
             // calculate the cost the user is paying, taking into account the users coupons
             $cost = $user->getCostFor($plan);
             $cycle = $plan->cycle;
